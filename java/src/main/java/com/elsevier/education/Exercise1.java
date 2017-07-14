c7d9069cf65b73b767a153a85841bea31368e7e4
kulswapnil <kulswapnil@gmail.com>
2017-07-14 18:39:08 -0400

    Update Exercise1.java
    Making this class as immutable.

--- java/src/main/java/com/elsevier/education/Exercise1.java
+++ java/src/main/java/com/elsevier/education/Exercise1.java
@@ -6,37 +6,41 @@ import java.util.Set;
 
 TODO: Make this class immutable.
 
-*/
-public class Exercise1 {
+/**
+ * 
+ * @author swapnilkulkarni
+ * Make the class as final 
+ * Make all field as private & final
+ * Initize those fields in default constructor.
+ * Remove all set methods
+ * return collection<String> so that collection can't be modified.
+ */
 
-	public static class Person {
+public final class Exercise1 {
+
+	public static final class Person {
 		
-		private Set<String> phoneNumbers;
-		private String firstName;
-		private String lastName;
+		private final Set<String> phoneNumbers;
+		private final String firstName;
+		private final String lastName;
 		
 		public Person() {
+			phoneNumbers = new HashSet<String>(Arrays.asList("267 844 5443", "234354"));
+			firstName = "Swapnil";
+			lastName = "Kulkarni";
 		}
 
-		public Set<String> getPhoneNumbers() {
-			return phoneNumbers;
+		public Collection<String> getPhoneNumbers() {
+			return Collections.unmodifiableCollection(phoneNumbers);
 		}
-		public void setPhoneNumbers(Set<String> newPhoneNumbers) {
-			phoneNumbers = newPhoneNumbers;
-		}
-		
+				
 		public String getFirstName() {
 			return firstName;
 		}
-		public void setFirstName(String newName) {
-			firstName = newName;
-		}
-		
+				
 		public String getLastName() {
 			return lastName;
 		}
-		public void setLastName(String newName) {
-			lastName = newName;
-		}
+		
 	}
 }
